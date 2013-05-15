2b78b4ee77d8a7a27f8b2482eb8a12f772b15d66
ricogallo <riccardo.gueli.alletti@gmail.com>
2013-05-15 14:46:21 +0200

    Add: angular.meucci module and meucci service into the client

--- src/client/intro.js
+++ src/client/intro.js
@@ -3,7 +3,12 @@
     exports = module.exports = factory(require('socket.io-client'), require('q'));
   } else if (typeof define === 'function' && define.amd) {
     define('meucci', ['io', 'Q'], factory);
+  } else if (typeof angular !== 'undefined' && angular.injector) {
+    angular.module('angular.meucci', [])
+      .factory('meucci', ['$window', '$q', function ($window, $q) {
+        return factory($window.io, $q);
+      }]);
   } else {
-    root.protocol = factory(root.io, root.Q);
+    root.meucci = factory(root.io, root.Q);
   }
 }(this, function (io, Q) {
