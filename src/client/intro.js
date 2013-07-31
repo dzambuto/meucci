98aa54ba91c7c421ff1bbea3f14fabfaa503974a
ricogallo <riccardo.gueli.alletti@gmail.com>
2013-07-31 05:50:01 +0200

    Angular support updated, now it yields an Angular Provider

--- src/client/intro.js
+++ src/client/intro.js
@@ -4,10 +4,19 @@
   } else if (typeof define === 'function' && define.amd) {
     define('meucci', ['io', 'Q'], factory);
   } else if (typeof angular !== 'undefined' && angular.injector) {
-    angular.module('angular.meucci', [])
-      .factory('meucci', ['$window', function ($window) {
-        return factory($window.io, $window.Q);
-      }]);
+    angular.module('meucci.angular', [])
+      .provider('meucci', function () {
+        var address = 'http://localhost'
+          , client = factory(root.io, root.Q);
+
+        this.setAddress = function(newAddress) {
+          address = newAddress;
+        }
+
+        this.$get = function() {
+          return client.connect(address);
+        }
+      });
   } else {
     root.meucci = factory(root.io, root.Q);
   }
